5c48776f1bb9aeba5700e116f39561484bdf4f56
Arvind Yadav <177812146+Arvind8454@users.noreply.github.com>
2025-09-06 18:29:17 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,26 @@
+// let a = 10
+// const b = 200
+// var c = 300
+let a = 200
+if(true){
+    let a = 10
+const b = 200
+
+console.log("INNER: ", a);
+function addNum(){
+    
+}
+
+} 
+
+for(let i = 0; i< Array.length; i++)
+{
+    const element = array[i];
+}
+
+
+
+
+// console.log(a);
+// console.log(b);
+console.log(a);
