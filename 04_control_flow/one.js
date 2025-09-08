91ca8c3f38e38afc9bc91c67f022b10d96d4bb03
Arvind Yadav <177812146+Arvind8454@users.noreply.github.com>
2025-09-08 18:29:58 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+// iff
+
+// const isUserLoggedIn = true
+
+// const temperature = 41
+
+// if( temperature === 41){
+//     console.log("Less than 50");
+
+// } else{
+//     console.log("temprature is grater than 50")
+// }
+
+// <, >, <=, >=, ==, !=, ====, !==
+
+
+const score = 200
+let power;
+
+if (score > 100) {
+    let power = "fly"
+    // console.log(`user power: ${power}`);
+}
+// console.log(`user power: ${power}`);
+
+
+// const  balance = 1000
+
+// if(balance > 500) console.log("test") , console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900){
+//     console.log("less than 750");
+
+// }else {
+//     console.log("less than 1200");
+// }
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if(isUserLoggedIn && debitCard && 2==3)// output kucch nhi ayega 
+if(isUserLoggedIn && debitCard && 2==2){
+    console.log(" Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail ){
+    console.log("User logged in");
+
+}
+
+
+
