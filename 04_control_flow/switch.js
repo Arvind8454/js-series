78e83b8af6e8bf006d3ffe20349a9a17f6f2119b
Arvind Yadav <177812146+Arvind8454@users.noreply.github.com>
2025-09-08 19:37:24 +0000

    swi

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+// switch (key) {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan";
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
